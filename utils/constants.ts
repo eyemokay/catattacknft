3bde0e9458225309556e7be3a7848b48fa8ad0b6
carnach <carnach@users.noreply.github.com>
2024-02-26 08:40:25 +0000

    Update constants.ts

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,5 +2,5 @@ import { BaseSepoliaTestnet } from "@thirdweb-dev/chains";
 
 export const CLIENT_ID = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "";
 export const CHAIN = BaseSepoliaTestnet;
-export const CONTRACT_ADDR = "0x5cA3b8E5B82D826aF6E8e9BA9E4E8f95cbC177F4";
-export const FACTORY_ADDR = "0x7b5ba9D46b53aae55e2c2E9b38d9AfF9a0b158F8";
+export const CONTRACT_ADDR = "0x1EF6e096C2637c8917C83743793b9E06c00Cd8DF";
+export const FACTORY_ADDR = "0x1cF94bC28adE6Ef534AB4Ed7B48cEF55747c5CF2";
